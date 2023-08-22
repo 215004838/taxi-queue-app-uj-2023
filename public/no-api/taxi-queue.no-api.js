28177d9561ddf7672d6c2fe17e160a14ed3e9418
mancoba <mtungwacornelius@gmail.com>
2023-08-22 12:02:34 +0200

    commito

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -4,26 +4,41 @@ document.addEventListener('alpine:init', () => {
 
 		return {
 			version: 'no-api-1.0',
+			passengersinqueue: 0,
+			taxisQueue: 0,
+
 			joinQueue() {
+				this.passengersinqueue++;
 
 			},
 			leaveQueue() {
+				if (this.passengersinqueue > 0) {
+					this.passengersinqueue--;
+				}
 
 			},
 
 			joinTaxiQueue() {
+				this.taxisQueue++;
 
 			},
 
 			queueLength() {
+				return this.passengersinqueue;
+
 
 			},
 
 			taxiQueueLength() {
+				return this.taxisQueue;
 
 			},
 
 			taxiDepart() {
+				if (this.passengersinqueue >= 12 && this.taxisQueue > 0) {
+					this.taxisQueue--;
+					this.passengersinqueue -= 12;
+				}
 
 			}
 		}
@@ -31,5 +46,3 @@ document.addEventListener('alpine:init', () => {
 	});
 
 });
-
-
